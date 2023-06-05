16bdd644d764270275b25863466d605099d4bfc5
David Lorenz <info@activenode.de>
2023-06-05 13:29:22 +0200

    update

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   integrations: [tailwind()],
 });
